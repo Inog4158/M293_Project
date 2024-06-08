73346e6bdd1c1355b82a86d36a2162371f293d30
Inog <inog@bluewin.ch>
2024-06-08 11:52:17 +0200

    feat: header, footer, home

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const items = document.querySelectorAll('.item');
+    const home = document.getElementById('home-item');
+    items.forEach(item => {
+        item.addEventListener('mouseover', function() {
+            if (item !== home.parentElement){
+                home.style.color = 'var(--color-text)';
+            }
+        });
+        item.addEventListener('mouseout', function() {
+            home.style.color = 'var(--color-highlight)';
+        });
+    });
+});
+
+window.addEventListener('scroll', function() {
+    var bild = document.getElementById('profile');
+    var scrollArea = 1000 -  window.innerHeight;
+    var scrollTop = window.scrollY || window.scrollTop;
+    var scrollPercent = scrollTop/scrollArea || 0;
+    bild.style.top = scrollPercent  * 15 + 'vh';
+});
