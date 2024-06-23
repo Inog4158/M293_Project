2e2ca92d2280739a4a8e073ca67a957f51013466
Inog <inog@bluewin.ch>
2024-06-23 14:07:02 +0200

    fix: javascript put mobile check inside event-listener and resets offset if mobile

--- js/script.js
+++ js/script.js
@@ -45,8 +45,9 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 });
 
-if (!isMobile()) {
-    window.addEventListener('scroll', function () {
+
+window.addEventListener('scroll', function () {
+    if (!isMobile()) {
         const hamburger = document.getElementById('hamburger-overlay');
         if (window.scrollY > document.getElementById('home').offsetHeight || isTablet()) {
             hamburger.classList.add('visible');
@@ -57,69 +58,76 @@ if (!isMobile()) {
             hamburger.classList.remove('visible');
             document.getElementById('sidebar').classList.remove('open');
         }
-    });
+    }
+});
 
-    window.addEventListener('resize', function () {
+window.addEventListener('resize', function () {
+    if (!isMobile()) {
         centerContact()
-    })
+    }
+})
 
-    window.addEventListener('scroll', function () {
-
+window.addEventListener('scroll', function () {
+    if (!isMobile()) {
         parallexEffect(document.getElementById('about-section'), -2, 90);
-        if(isTablet()) {
+        if (isTablet()) {
             parallexEffect(document.getElementById('skills-section'), -0.8, 21);
-        }
-        else {
+        } else {
             parallexEffect(document.getElementById('skills-section'), -1, 0);
         }
-        
+
         document.getElementById('profile').style.top = getScrollOffsetter(1.5, -5, 'svh');
-    });
+    }
+    else {
+        document.getElementById('about-section').style.backgroundPositionY = '0';
+        document.getElementById('skills-section').style.backgroundPositionY = '0';
+    }
+});
+
+
+
+/*Functions*/
+function centerContact() {
+    const contact = document.getElementById('contact-section');
+    const puffer = document.getElementById('puffer-section');
+    var contactHeight = parseFloat((getStyleValue(contact, 'padding-bottom').slice(0, -2))) + parseFloat(getStyleValue(contact, 'height').slice(0, -2))
+    contact.style.bottom = (getStyleValue(puffer, 'height').slice(0, -2)) - contactHeight / 2 + 'px';
+}
+
+function getStyleValue(elm, property) {
+    return window.getComputedStyle(elm).getPropertyValue(property);
+}
+
+function getVerticalScrollPercentage() {
+    const elm = document.body;
+    const p = elm.parentNode
+    return (elm.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight) * 100
+}
+
+function getScrollOffsetter(multiplier, offset, unit) {
+    return getVerticalScrollPercentage() * multiplier + offset + unit;
+}
+
+function parallexEffect(elm, multiplier, offset) {
+    elm.style.backgroundPositionY = getScrollOffsetter(multiplier, offset, 'svh')
 }
 
 
-    /*Functions*/
-    function centerContact() {
-        const contact = document.getElementById('contact-section');
-        const puffer = document.getElementById('puffer-section');
-        var contactHeight = parseFloat((getStyleValue(contact, 'padding-bottom').slice(0, -2))) + parseFloat(getStyleValue(contact, 'height').slice(0, -2))
-        contact.style.bottom = (getStyleValue(puffer, 'height').slice(0, -2)) - contactHeight / 2 + 'px';
+/*Form*/
+function send() {
+    let nameField = document.getElementById("input-name");
+    let emailField = document.getElementById("input-email");
+    let messageField = document.getElementById("input-message");
+    if (nameField.value !== "" && emailField.value !== "" && messageField.value !== "") {
+        window.alert("name:\n" + nameField.value + "\nemail:\n" + emailField.value + "\nmessage: \n" + messageField.value);
+        document.getElementById("form").reset();
     }
+}
 
-    function getStyleValue(elm, property) {
-        return window.getComputedStyle(elm).getPropertyValue(property);
-    }
+function isMobile() {
+    return window.matchMedia("screen and (max-width: 767px)").matches;
+}
 
-    function getVerticalScrollPercentage() {
-        const elm = document.body;
-        const p = elm.parentNode
-        return (elm.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight) * 100
-    }
-
-    function getScrollOffsetter(multiplier, offset, unit) {
-        return getVerticalScrollPercentage() * multiplier + offset + unit;
-    }
-
-    function parallexEffect(elm, multiplier, offset) {
-        elm.style.backgroundPositionY = getScrollOffsetter(multiplier, offset, 'svh')
-    }
-
-
-    /*Form*/
-    function send() {
-        let nameField = document.getElementById("input-name");
-        let emailField = document.getElementById("input-email");
-        let messageField = document.getElementById("input-message");
-        if (nameField.value !== "" && emailField.value !== "" && messageField.value !== "") {
-            window.alert("name:\n" + nameField.value + "\nemail:\n" + emailField.value + "\nmessage: \n" + messageField.value);
-            document.getElementById("form").reset();
-        }
-    }
-
-    function isMobile() {
-        return window.matchMedia("screen and (max-width: 767px)").matches;
-    }
-    
-    function isTablet() {
-        return window.matchMedia("screen and (max-aspect-ratio: 15 / 11) and (min-width: 767px)").matches;
-    }
+function isTablet() {
+    return window.matchMedia("screen and (max-aspect-ratio: 15 / 11) and (min-width: 767px)").matches;
+}
